10c6f950aec6ead2354693e1f1b553c987a7582e
Roman Mosiienko <roma.mosiienko777@gmail.com>
2018-07-19 19:03:05 +0300

    Translation of layout into template

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,24 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/prettyPhoto.css',
+        'css/price-range.css',
+        'css/animate.css',
+        'css/main.css',
+        'css/responsive.css',
     ];
     public $js = [
+//        'js/jquery.j',
+//        'js/bootstrap.min.j',
+        'js/jquery.scrollUp.min.js',
+        'js/price-range.j',
+        'js/jquery.prettyPhoto.j',
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
--- config/web.php
+++ config/web.php
@@ -7,14 +7,16 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'KXkeHNTp3QxEpptcBqpWV6Zuxrm3tLkq',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
